d9283fd8643556ddcd8054a06fddaa10b9e8a7c7
Susu <aslan.erdal.dev@gmail.com>
2023-01-02 16:21:58 +0100

    [Feature] Payment page and Cart are created. - Payment html page structure is created and cutted in components - Cart html page structure is created and cutted in components.

---
+++ src/app/components/payment-credit-card/payment-credit-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-credit-card',
+  templateUrl: './payment-credit-card.component.html',
+  styleUrls: ['./payment-credit-card.component.css']
+})
+export class PaymentCreditCardComponent {
+
+}
