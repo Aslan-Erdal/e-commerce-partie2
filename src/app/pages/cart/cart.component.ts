43ffd78e7c1134d43ea6f909903c4c27c8cbe523
Susu <aslan.erdal.dev@gmail.com>
2023-01-03 15:44:06 +0100

    [Feature] Cart Css - Css of Cart page done.

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+
+  constructor(private cart: CartService) { }
+
+  ngOnInit(): void {
+    this.cart.getCart();
+  }
+
+
 
 }
---
+++ src/app/services/cart/cart.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  constructor() { }
+
+  public getCart() {
+
+    const cart = localStorage.getItem("cart");
+
+    if(cart) {
+      return JSON.parse(cart);
+    } else {
+      this.createCart();
+      this.getCart();
+    }
+
+  }
+
+  private createCart() {
+
+    const newCart = JSON.stringify([]);
+    localStorage.setItem('cart', newCart);
+
+  }
+
+  private saveCart() {
+
+  }
+
+  public addProductToCart() {
+
+  }
+
+  public removeProductFromCart() {
+
+  }
+
+
+}
