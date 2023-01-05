026ce14e6a273639de442adc63b388a64e0147db
Susu <aslan.erdal.dev@gmail.com>
2023-01-05 16:22:27 +0100

    [Feature] Payment page done - Delivery address forms are done with Validators. - Credit card forms done with Validators. - when click on card submit you must be redirected on success page.

--- src/app/components/delivery-address/delivery-address.component.ts
+++ src/app/components/delivery-address/delivery-address.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -10,22 +10,27 @@ import { Router } from '@angular/router';
 export class DeliveryAddressComponent implements OnInit {
   public paymentForm!: FormGroup;
   public validationErrors: string[] = [];
+  public dataFromStorage: any;
 
   constructor(private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
-    this.sendDeliveryAddress();
+     this.sendDeliveryAddress();
+     this.initLocal();
   }
 
+  initLocal() {
+    this.dataFromStorage = this.getItemFromStorage();
+  }
 
   public sendDeliveryAddress() {
     this.paymentForm = this.formBuilder.group({
       delivery_prenom: [null, [Validators.required, Validators.minLength(3)]],
-      delivery_nom: [null, [Validators.required]],
+      delivery_nom: [null, [Validators.required, Validators.minLength(3)]],
       delivery_portable: [null, [Validators.required, Validators.minLength(10)]],
       delivery_email: [null, [Validators.required, Validators.email]],
       delivery_address: [null, [Validators.required]],
-      delivery_pays: [null, [Validators.required]],
+      delivery_pays: [null, [Validators.required, Validators.minLength(4)]],
     });
   }
 
@@ -37,20 +42,29 @@ export class DeliveryAddressComponent implements OnInit {
     console.log(this.paymentForm.controls);
 
     if (this.paymentForm.invalid) {
+      return;
       Object.keys(this.paymentForm.controls).forEach(control => {
         const currentInput = this.paymentForm.get(control);
         if(currentInput && currentInput.status === "INVALID") {
           this.validationErrors.push(control);
         }
-
         console.log(control, currentInput);
       });
     } else {
-      this.router.navigate(['/payment-success']);
+      console.log(this.paymentForm.value);
+      this.addItemToStorage(JSON.stringify(this.paymentForm.value));
+      this.initLocal();
+     // this.router.navigate(['/payment-success']);
     }
   }
 
+  public addItemToStorage(item: string) {
+    localStorage.setItem('address', item);
+  }
 
+  public getItemFromStorage() {
+    return JSON.parse(localStorage.getItem('address')!)
+  }
 
   isValidInput(fieldName: string): boolean {
     return (
@@ -61,6 +75,11 @@ export class DeliveryAddressComponent implements OnInit {
   }
 
 
-
+  get prenom() { return this.paymentForm.get('delivery_prenom')!.errors; }
+  get nom() { return this.paymentForm.get('delivery_nom')!.errors; }
+  get portable() { return this.paymentForm.get('delivery_portable')!.errors; }
+  get email() { return this.paymentForm.get('delivery_email')!.errors; }
+  get address() { return this.paymentForm.get('delivery_address')!.errors; }
+  get pays() { return this.paymentForm.get('delivery_pays')!.errors; }
 
 }
--- src/app/components/payment-credit-card/payment-credit-card.component.ts
+++ src/app/components/payment-credit-card/payment-credit-card.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-payment-credit-card',
   templateUrl: './payment-credit-card.component.html',
   styleUrls: ['./payment-credit-card.component.css']
 })
-export class PaymentCreditCardComponent {
+export class PaymentCreditCardComponent implements OnInit{
+  public cardForm!: FormGroup;
+
+  public errorPayMessage!: string;
+
+  months = ['Mois', '01','02', '03','04', '05', '06', '07', '08', '09', '10', '11', '12',];
+  years = ['Année', '2023', '2024', '2025', '2026', '2027', '2028', '2029',];
+
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
+
+  ngOnInit(): void {
+    this.initCardForm();
+  }
+
+  public initCardForm() {
+    this.cardForm = this.formBuilder.group({
+      num_card: [null, [Validators.required]],
+      month: [this.months[0], [Validators.required]],
+      year: [this.years[0], [Validators.required]],
+      cvv: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(3)]],
+
+    });
+  }
+
+  public onPay(): void  {
+    if(this.cardForm.invalid) {
+      this.errorPayMessage = "Veuillez renseigner tous les champs!";
+    } else {
+      this.errorPayMessage = "";
+      this.router.navigate(['/payment-success']);
+    }
+    console.log(this.cardForm.value);
+    console.log("CARDFORM : ", this.cardForm);
+  }
+
+
+  public getItemFromStorage() {
+    return JSON.parse(localStorage.getItem('address')!)
+  }
+
+  isValidInput(fieldName: string): boolean {
+    return (
+      this.cardForm.controls[fieldName].invalid  &&
+      (this.cardForm.controls[fieldName].dirty ||
+        this.cardForm.controls[fieldName].touched)
+    );
+  }
+
+  get num_card() { return this.cardForm.get('num_card')!.errors; }
+  get month() { return this.cardForm.get('month')!.errors; }
+  get year() { return this.cardForm.get('year')!.errors; }
+  get cvv() { return this.cardForm.get('cvv')!.errors; }
 
 }
