3bc52d5817eefc28b5eb0a771765307dd170ca5a
bkenzia <bkenzia@yahoo.fr>
2023-01-05 17:21:27 +0100

    le carousel

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from 'src/app/mocks/products.mock';
+import { ProductsService } from 'src/app/services/products/products.service';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
 })
 export class CarouselComponent {
+  productsPromos: Product[] = [];
+  constructor(
+    private productsService: ProductsService,
+    private router: Router
+  ) {}
+  ngOnInit() {
+    this.getProductsPromos();
+    // console.log('tous les produit', this.productsService.getProducts());
+    this.carouselAnimate();
+  }
 
+  getProductsPromos(): void {
+    const productsFound = this.productsService.getPromos();
+    if (productsFound) {
+      this.productsPromos = productsFound;
+    } else {
+      this.router.navigate(['not-found']);
+    }
+  }
+
+  // getPrixPromos() {
+  //   this.productsService.getPrixPromos();
+  // }
+
+  carouselAnimate() {
+    const productContainers = document.querySelectorAll('.product-container');
+    const productContainersArray =
+      Array.prototype.slice.call(productContainers);
+    const nxtBtn = document.querySelectorAll('.nxt-btn');
+    const nxtBtnArray = Array.prototype.slice.call(nxtBtn);
+    const preBtn = document.querySelectorAll('.pre-btn');
+    const preBtnArray = Array.prototype.slice.call(preBtn);
+
+    productContainersArray.forEach((item, i) => {
+      let containerDimensions = item.getBoundingClientRect();
+      let containerWidth = containerDimensions.width / 2;
+      nxtBtnArray[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+      });
+
+      preBtnArray[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+      });
+    });
+  }
 }
