f139be9caed2ce955acf0bebca6334544dabf453
bkenzia <bkenzia@yahoo.fr>
2023-01-16 12:06:01 +0100

    la css product-details et la fonction search dans component header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Product, PRODUCTS } from 'src/app/mocks/products.mock';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-
+  Products: Product[] = [];
+  formSearch!: FormGroup;
+  keys = '';
+  constructor(private formBuilder: FormBuilder) {}
+  ngOnInit() {
+    this.formSearch = this.formBuilder.group({
+      search: [null],
+    });
+  }
+  getInseedCode() {
+    console.log(this.formSearch.value.search);
+  }
+  recherche() {
+    let input = this.formSearch.value.search;
+    const searchProducts = PRODUCTS.filter(
+      (product) =>
+        product.categorie.includes(input) ||
+        product.sousCategorie.includes(input)
+    );
+    return searchProducts;
+  }
 }
